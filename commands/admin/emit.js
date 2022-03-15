fbbc64fa9c80d9bd2fb5c5d2d385f34c2ed26d98
agon deda <kxiszrr@gmail.com>
2022-03-15 16:27:09 +0100

    emit

---
+++ commands/admin/emit.js
@@ -0,0 +1,46 @@
+module.exports = {
+    name: 'emit',
+    description: 'emettre un evenement',
+    run: (client, message, args) => {
+        if (!args[0] || !args[0].match(/^(guildMemberAdd|guildMemberRemove)$/)) return message.reply('Merci d\'entrer un événement valide (\`guildMemberAdd\`/\`guildMemberRemove\`)');
+
+        if (args[0] == 'guildMemberAdd') {
+            client.emit('guildMemberAdd', message.member);
+            message.reply('Event guildMemberAdd émit!');
+        } else {
+            client.emit('guildMemberRemove', message.member);
+            message.reply('Event guildMemberRemove émit!');
+        }
+    },
+    options: [
+        {
+            name: 'event',
+            description: 'choisir un evenement',
+            type: 'STRING',
+            required: true,
+            choices: [
+                {
+                    name: 'guildMemberAdd',
+                    value: 'guildMemberAdd'
+                },
+                {
+                    name: 'guildMemberRemove',
+                    value: 'guildMemberRemove'
+                }
+            ]
+        }
+    ],
+    
+    runSlash: (client, interaction) => {
+        const evtChoices = interaction.options.getString('event');
+
+        if (evtChoices == 'guildMemberAdd') {
+            client.emit('guildMemberAdd', interaction.member);
+            interaction.reply({ content: 'Event guildMemberAdd émit!', ephemeral: true });
+        } else {
+            client.emit('guildMemberRemove', interaction.member);
+            interaction.reply({ content: 'Event guildMemberRemove émit!', ephemeral: true });
+        }
+    }
+    
+};
