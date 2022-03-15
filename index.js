94a3f84834da935cc7072267cb6c4f3c6060ba6a
agon deda <kxiszrr@gmail.com>
2022-03-15 15:24:20 +0100

    database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client, Collection } = require('discord.js');
 const dotenv = require('dotenv'); dotenv.config();
 const client = new Client({ intents: 513 });
+const mongoose = require('mongoose')
 
 client.commands = new Collection();
 
@@ -11,5 +12,14 @@ process.on('exit', code => { console.log(`Le processus s'est arreter avec le cod
 process.on('uncaughtException', (err, origin) => { console.log(`UNCAUGHT_EXCEPTION: ${errr}`, `Origine: ${origin}`)});
 process.on('unhandledRejection', (reason, promise) => { console.log(`UNHANDLED_REJECTIO: ${reason}\n-----\n , promise`)});
 process.on('warning', (...args) => console.log(...args));
+
+mongoose.connect(process.env.DATABASE_URI, {
+    autoIndex: false, 
+    maxPoolSize: 10, 
+    serverSelectionTimeoutMS: 5000, 
+    socketTimeoutMS: 45000, 
+    family: 4 
+}).then(() => { console.log('Le client est connecté à la base de données ! ') })
+.catch(err => { console.log(err) })
 
 client.login(process.env.TOKEN);
